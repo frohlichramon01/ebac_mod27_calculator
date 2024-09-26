7f69220f89a3382915bcb35e3b125bd0cc8190b2
frohlichramon01 <frohlichramon01@gmail.com>
2024-09-26 10:08:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Olá!</h1>
+    </header> 
+
+  </div>
+
+
+  
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
